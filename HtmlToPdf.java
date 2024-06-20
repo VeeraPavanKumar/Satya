e958db94d687977f7102af9cf9c987cbecf13979
VeeraPavanKumar <131237944+VeeraPavanKumar@users.noreply.github.com>
2024-06-20 07:46:43 +0530

    Add files via upload

--- HtmlToPdf.java
+++ HtmlToPdf.java
@@ -1,19 +1,27 @@
 package s1;
 
+
+import com.itextpdf.html2pdf.ConverterProperties;
+import com.itextpdf.html2pdf.HtmlConverter;
+import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.layout.font.FontProvider;
+import com.itextpdf.layout.renderer.DocumentRenderer;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.PdfDocument;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import com.itextpdf.html2pdf.ConverterProperties;
-import com.itextpdf.html2pdf.HtmlConverter;
-
 public class HtmlToPdf {
 
     public static void main(String[] args) {
-        String inputHtmlFilePath = "C:/htmltopdfusingjsoup/Form1.html";
+        String inputHtmlFilePath = "C:/htmltopdfusingjsoup/satya.html";
         String outputPdfFilePath = "C:/htmltopdfusingjsoup/satya.pdf";
-        String baseUri = "file:/C:/htmltopdfusingjsoup/";
+        String baseUri = "file:///C:/htmltopdfusingjsoup/";
 
         try {
             // Read HTML content from file
@@ -31,7 +39,18 @@ public class HtmlToPdf {
         try (FileOutputStream outputStream = new FileOutputStream(outputPdfFilePath)) {
             ConverterProperties converterProperties = new ConverterProperties();
             converterProperties.setBaseUri(baseUri);
-            HtmlConverter.convertToPdf(htmlContent, outputStream, converterProperties);
+
+            FontProvider fontProvider = new FontProvider();
+            fontProvider.addStandardPdfFonts();
+            fontProvider.addSystemFonts();
+            converterProperties.setFontProvider(fontProvider);
+
+            PdfWriter writer = new PdfWriter(outputPdfFilePath);
+            PdfDocument pdfDoc = new PdfDocument(writer);
+            pdfDoc.setDefaultPageSize(PageSize.A3.rotate());
+            Document document = new Document(pdfDoc);
+            HtmlConverter.convertToPdf(htmlContent, pdfDoc, converterProperties);
+            document.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
